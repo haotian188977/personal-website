8e6edbf4ac35f64e8c212d3d4d6d8259fe596879
haotian188977 <haotian188977@gmail.com>
2026-07-24 20:47:48 +0800

    image_fix

---
+++ app/Link/page.tsx
@@ -0,0 +1,85 @@
+export default function Home() {
+    return (
+    	<main className="relative min-h-screen bg-blue-50 overflow-hidden">
+	    <h1 className="text-center text-blue-900 text-8xl font-normal font-mono leading-loose" >Links</h1>
+	    <hr className="border-t border-blue-900 my-10" />
+            <div className="text-center">
+	        <a 
+		    href = "https://github.com/haotian188977/personal-website"
+		    target="_blank"
+		    className="
+		    	text-center
+		    	text-3xl
+		    	font-mono
+        	    	text-blue-600
+        	    	underline
+    		    "
+	    	>
+			The GitHub page of my website, the source codes and build history are included there
+	    	</a>
+	    </div>
+	    <div  className="flex justify-center mt-15">
+		<img
+                    src="/images/github.png"
+                    className="w-400 h-150 object-cover rounded-lg border"
+                />
+	    </div>
+	    <hr className="border-t border-blue-900 my-10" />
+	    <div className="text-center">
+	        <a 
+		    href = "https://sites.google.com/tcdsb.ca/haotian-portfolio/"
+		    target="_blank"
+		    className="
+		    	text-center
+		    	text-3xl
+		    	font-mono
+        	    	text-blue-600
+        	    	underline
+    		    "
+	    	>
+			A communication technology course website created by me, includes some of my coursework
+	    	</a>
+	    </div>
+	    <div  className="flex justify-center mt-15">
+		<img
+                    src="/images/com.png"
+                    className="w-400 h-150 object-cover rounded-lg border"
+                />
+	    </div>
+	    <div  className="flex justify-center">
+		<img
+                    src="/images/com2.png"
+                    className="w-400 h-150 object-cover rounded-lg border"
+                />
+	    </div>
+	    <div  className="flex justify-center">
+		<img
+                    src="/images/com3.png"
+                    className="w-400 h-650 object-cover rounded-lg border"
+                />
+	    </div>
+	    <hr className="border-t border-blue-900 my-10" />
+            <div className="text-center">
+	        <a 
+		    href = "https://sites.google.com/tcdsb.ca/haotian-gan/"
+		    target="_blank"
+		    className="
+		    	text-center
+		    	text-3xl
+		    	font-mono
+        	    	text-blue-600
+        	    	underline
+    		    "
+	    	>
+			A digital art course website created by me, includes some of my coursework
+	    	</a>
+	    <div  className="flex justify-center mt-15">
+		<img
+                    src="/images/art.png"
+                    className="w-400 h-200 object-cover rounded-lg border"
+                />
+	    </div>
+	    </div>
+    	</main>
+    );
+}
---
+++ app/page.tsx
@@ -0,0 +1,28 @@
+export default function Home() {
+    return (
+	<main className="relative min-h-screen bg-blue-500 overflow-hidden">
+	    <div className="
+    		flex
+    		items-center
+    		gap-10
+		mt-20
+	    ">
+		<img
+        	    src="/images/test.jpg"
+       		    className="
+			ml-20
+            		w-96
+            		h-130
+           		rounded-full
+            		object-cover
+        	    "
+    		/>
+
+		<div>
+	    	    <h1 className="ml-20 mr-20 leading-loose text-front text-white text-7xl font-black font-mono leading-loose" > Hi, my name is Haotian </h1>
+	    	    <h1 className="ml-20 mr-50 leading-normal text-front text-white text-5xl font-semibold font-mono leading-none" > a student studied in China and Canada with multi-culture environments, interested in computer science, programming, and technology, currently woring toward the goal of becoming a computer engineer </h1>
+		</div>
+	    </div>
+        </main>
+    );
+}
